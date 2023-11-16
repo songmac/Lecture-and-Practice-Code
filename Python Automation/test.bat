d2311273373c962f477710825611a524aa7e6edf
Suyeon <ssy4784@naver.com>
2023-11-16 17:07:40 +0900

    new : Python Automation : code

---
+++ Python Automation/test.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:\Users\User\anaconda3\python.exe" "C:\Users\User\project\SESAC\2023-Sesac-Lecture-and-Project\Python Automation\test.py"
---
+++ Python Automation/test.py
@@ -0,0 +1,2 @@
+with open(r'C:/Users/User/project/SESAC/2023-Sesac-Lecture-and-Project/Python Automation/test.json','w'):
+    pass
